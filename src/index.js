3a51f3d50b9fdcbb126369cdbe317b12b69254f4
ayeman <ayemanbsalauddin@gmail.com>
2021-11-25 11:57:16 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
+
+import { StateContextProvider } from "./Context/ContextProvider";
+
+import App from "./App";
+import "./global.css";
+
+reactDom.render(
+  <StateContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </StateContextProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
