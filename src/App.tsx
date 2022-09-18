14628e4f0d6bc154b1e22886c3256133a2dbc033
Ayeman <ayemanbsalauddin@gmail.com>
2022-09-18 19:31:15 +0600

    Porting Giggle to React18, Router6, Vite, MUI, Sass

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae
+      voluptatum, dolores nihil saepe dicta id quasi, laboriosam, quos
+      accusantium explicabo tempora mollitia asperiores sit quaerat beatae
+      suscipit nobis quod. Recusandae?
+    </div>
+  );
+};
+
+export default App;
